aae43be8ba2c3221c4b4a08a3c536c78c095f2f9
Tamir Twina <tamir@houzz.com>
2020-01-13 02:50:08 +0200

    Fix remaining tasks calculation

--- jobs/board_status.rb
+++ jobs/board_status.rb
@@ -3,13 +3,15 @@ require 'net/http'
 require 'net/https'
 
 class BoardStatus
-  def initialize(unbreak_now, needs_triage, high, normal,low,done,all_tasks)
+  def initialize(unbreak_now, needs_triage, high, normal,low,open_tasks,done_tasks,tasks_to_verify,all_tasks)
     @unbreak_now = unbreak_now
     @needs_triage = needs_triage
     @high = high
     @normal = normal
     @low = low
-    @done = done
+    @open_tasks = open_tasks
+    @tasks_to_verify = tasks_to_verify
+    @done_tasks = done_tasks
     @all_tasks = all_tasks
   end
   def unbreak_now
@@ -27,8 +29,14 @@ class BoardStatus
   def low
     @low
   end
-  def done
-    @done
+  def open_tasks
+    @open_tasks
+  end
+  def tasks_to_verify
+    @tasks_to_verify
+  end
+  def done_tasks
+    @done_tasks
   end
   def all_tasks
     @all_tasks
@@ -77,25 +85,29 @@ end
 
 
 def get_status(url) 
-  tasks = get_all_tasks(url)
-  remaining_tasks = tasks.select { |task| !["accepted", "resolved"].include?(task['fields']['status']['value'])}
-  unbreak_now = remaining_tasks.select { |task| task['fields']['priority']['value'] == 100 }
-  needs_triage = remaining_tasks.select { |task| task['fields']['priority']['value'] == 90 }
-  high = remaining_tasks.select { |task| task['fields']['priority']['value'] == 80 }
-  normal = remaining_tasks.select { |task| task['fields']['priority']['value'] == 50 }
-  low = remaining_tasks.select { |task| task['fields']['priority']['value'] < 50 }
-  done = tasks.select { |task| ["accepted", "resolved"].include?(task['fields']['status']['value'])}
-  status = BoardStatus.new(unbreak_now,needs_triage,high,normal,low,done,tasks)
+  all_tasks = get_all_tasks(url)
+  open_tasks = all_tasks.select { |task| ["open","reopened","inprogress","onhold"].include?(task['fields']['status']['value'])}
+  unbreak_now = open_tasks.select { |task| task['fields']['priority']['value'] == 100 }
+  needs_triage = open_tasks.select { |task| task['fields']['priority']['value'] == 90 }
+  high = open_tasks.select { |task| task['fields']['priority']['value'] == 80 }
+  normal = open_tasks.select { |task| task['fields']['priority']['value'] == 50 }
+  low = open_tasks.select { |task| task['fields']['priority']['value'] < 50 }
+  done_tasks = all_tasks.select { |task| ["accepted","duplicate","invalid","notreproduce","patched","wontfix", "resolved"].include?(task['fields']['status']['value'])}
+  tasks_to_verify = all_tasks.select { |task| ["resolved"].include?(task['fields']['status']['value'])}
+  puts open_tasks.size.to_f
+  puts done_tasks.size.to_f
+  puts all_tasks.size.to_f
+  status = BoardStatus.new(unbreak_now, needs_triage, high, normal,low,open_tasks,done_tasks,tasks_to_verify,all_tasks)
   return status
 end
 
 def print_status(status) 
-  text = 'Critical: ' + status.unbreak_now.size.to_s + ' ' + 'Triage: ' + status.needs_triage.size.to_s + ' ' + 'High: ' + status.high.size.to_s + ' ' + 'Normal: ' + status.normal.size.to_s + ' ' + 'Low: ' + status.low.size.to_s + ' | ' + status.done.size.to_s + '/' + status.all_tasks.size.to_s + ' ' + (status.done.size.to_f / status.all_tasks.size.to_f).round(2).to_s
+  text = 'Critical: ' + status.unbreak_now.size.to_s + ' ' + 'Triage: ' + status.needs_triage.size.to_s + ' ' + 'High: ' + status.high.size.to_s + ' ' + 'Normal: ' + status.normal.size.to_s + ' ' + 'Low: ' + status.low.size.to_s + ' | ' + status.done_tasks.size.to_s + '/' + status.all_tasks.size.to_s + ' ' + (status.done_tasks.size.to_f / status.all_tasks.size.to_f).round(2).to_s
   return text
 end
 
 def progress_report(workboard,sprint_status)
-  closed_points = sprint_status.done.size || 0
+  closed_points = sprint_status.done_tasks.size || 0
   total_points = sprint_status.all_tasks.size || 0 
   if total_points == 0 then
       percentage = 0
@@ -128,7 +140,17 @@ def tasks_distribution_report(workboard,sprint_status)
   },]
   options = { }
 
-  send_event(workboard['id']+'-doughnutchart', { labels: labels, datasets: data, options: options })
+  send_event(workboard['id']+'-doughnutchart', { labels: labels_with_numbers(labels,sprint_status), datasets: data, options: options })
+end
+
+def labels_with_numbers (labels,sprint_status)
+  return [ 
+    "#{sprint_status.unbreak_now.size} #{labels[0]}",
+    "#{sprint_status.needs_triage.size} #{labels[1]}",
+    "#{sprint_status.high.size} #{labels[2]}",
+    "#{sprint_status.normal.size} #{labels[3]}",
+    "#{sprint_status.low.size} #{labels[4]} "
+  ]
 end
 
 
